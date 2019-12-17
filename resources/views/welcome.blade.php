8a9cfa1df19fd354b53d9baa5c61b7030f25d4a1
chandra <bargeosaputra@gmail.com>
2019-12-17 10:12:06 +0700

    Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Chandra Saputra
                 </div>
 
                 <div class="links">
